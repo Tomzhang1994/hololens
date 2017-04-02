9b18edd1ea41bf3d3e28b7aca828ad08f3c6fd96
Tomzhang1994 <1317809800@qq.com>
2017-04-02 16:13:45 +1200

    pre-production(test)
    这是基础cs文件，之后可能会改动或新增

---
+++ Hololens preproduction01/Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SphereCommands : MonoBehaviour
+{
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            GetComponent<Animator>().Play("Jumping@loop", 0, 0f);
+        }
+    }
+}
---
+++ Hololens preproduction01/Assets/readme.txt
@@ -0,0 +1,4 @@
+现阶段主要的（需要编辑的）文件就这几个，修改文本后替换原项目文件即可。
+Animation 文件夹主管动画，有animator
+HoloToolKit是微软的开发支援文件
+scripts放客制代码
